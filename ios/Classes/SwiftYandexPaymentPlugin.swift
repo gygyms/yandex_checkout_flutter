efd120fabda103dd9326c8b8cbeac5cba2ba5a46
gygyms <gygygyms@gmail.com>
2020-05-21 14:53:16 +0300

    Добавлен функционал для айос с ограничениями

--- ios/Classes/SwiftYandexPaymentPlugin.swift
+++ ios/Classes/SwiftYandexPaymentPlugin.swift
@@ -1,14 +1,76 @@
 import Flutter
 import UIKit
+import YandexCheckoutPayments
+import YandexCheckoutPaymentsApi
 
-public class SwiftYandexPaymentPlugin: NSObject, FlutterPlugin {
+extension UIViewController: TokenizationModuleOutput {
+    public func didSuccessfullyPassedCardSec(on module: TokenizationModuleInput) {
+    }
+    
+    public func tokenizationModule(_ module: TokenizationModuleInput,
+                          didTokenize token: Tokens,
+                          paymentMethodType: PaymentMethodType) {
+    DispatchQueue.main.async { [weak self] in
+        guard let self = self else { return }
+        self.dismiss(animated: true)
+    }
+    // Отправьте токен в вашу систему
+  }
+
+    public func didFinish(on module: TokenizationModuleInput,
+                 with error: YandexCheckoutPaymentsError?) {
+      DispatchQueue.main.async { [weak self] in
+          guard let self = self else { return }
+          self.dismiss(animated: true)
+      }
+  }
+}
+public class SwiftYandexPaymentPlugin: NSObject, FlutterPlugin, TokenizationModuleOutput {
+    public func didFinish(on module: TokenizationModuleInput, with error: YandexCheckoutPaymentsError?) {
+        
+    }
+    
+    public func didSuccessfullyPassedCardSec(on module: TokenizationModuleInput) {
+        
+    }
+    
+    public func tokenizationModule(_ module: TokenizationModuleInput, didTokenize token: Tokens, paymentMethodType: PaymentMethodType) {
+        
+    }
+    
+  
+    
+    
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "yandex_payment", binaryMessenger: registrar.messenger())
+    let channel = FlutterMethodChannel(name: "example.com/yandex", binaryMessenger: registrar.messenger())
     let instance = SwiftYandexPaymentPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
-  }
+    var data: [String: Any] = [:]
+    var args:Dictionary = call.arguments as! Dictionary<String, String>
+    data["paymentToken"]="iOS " + UIDevice.current.systemVersion;
+    data["paymentMethodType"]="BANK_CARD";
+    let clientApplicationKey = args["clientApplicationKey"]
+    let amount = Amount(value: 999.99, currency: .rub)
+    let tokenizationModuleInputData =
+        TokenizationModuleInputData(clientApplicationKey: clientApplicationKey!,
+                                          shopName: "Космические объекты",
+                                          purchaseDescription: """
+                                                               Комета повышенной яркости, период обращения — 112 лет
+                                                               """,
+                                          amount: amount,
+                                          isLoggingEnabled: true,
+                                          savePaymentMethod: .on)
+    let inputData: TokenizationFlow = .tokenization(tokenizationModuleInputData)
+    let viewController = TokenizationAssembly.makeModule(inputData: inputData,
+                                                         moduleOutput: self)
+    //viewController.present(viewController, animated: true, completion: nil)
+   /* if let navigationController = UIApplication.shared.keyWindow?.rootViewController as? UINavigationController {
+               navigationController.pushViewController(viewController, animated: true)
+           }
+  */ UIApplication.shared.delegate?.window??.rootViewController?.present(viewController, animated: true, completion: nil)
+    //result(data)
+    }
 }
--- ios/yandex_checkout.podspec
+++ ios/yandex_checkout.podspec
@@ -3,7 +3,7 @@
 #
 Pod::Spec.new do |s|
   s.name             = 'yandex_checkout'
-  s.version          = '0.0.1'
+  s.version          = '0.0.2'
   s.summary          = 'A Yandex Checkout plugin for Flutter.'
   s.description      = <<-DESC
 A Yandex Payment plugin for Flutter.
@@ -15,6 +15,7 @@ A Yandex Payment plugin for Flutter.
   s.source_files = 'Classes/**/*'
   s.public_header_files = 'Classes/**/*.h'
   s.dependency 'Flutter'
+  s.dependency 'YandexCheckoutPayments'
   s.vendored_frameworks = 'Frameworks/TMXProfiling.framework', 'Frameworks/TMXProfilingConnections.framework'
   s.ios.deployment_target = '8.0'
 end
