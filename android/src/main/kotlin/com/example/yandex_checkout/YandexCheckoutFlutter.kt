fe3ecd3682709e9550cdb386a351170d4adfc035
gygyms <gygygyms@gmail.com>
2020-05-28 10:20:39 +0300

    Реализована возможность подтверждения платежа по карте для android

--- android/src/main/kotlin/com/example/yandex_checkout/YandexCheckoutFlutter.kt
+++ android/src/main/kotlin/com/example/yandex_checkout/YandexCheckoutFlutter.kt
@@ -10,6 +10,7 @@ import java.util.*
 private const val CHANNEL = "example.com/yandex"
 private const val REQUEST_TOKENIZE_CODE = 99
 const val PAYMENT_CHANNEL_METHOD = "showYandexPayment"
+const val CONFIRMATION_CHANNEL_METHOD = "confirm"
 
 class YandexCheckoutFlutter {
     private var registrar: PluginRegistry.Registrar
@@ -31,6 +32,13 @@ class YandexCheckoutFlutter {
                 } catch (e: Exception) {
                     result.error(e.javaClass.name, e.message, e)
                 }
+                CONFIRMATION_CHANNEL_METHOD -> try {
+                    channelResult = result
+                    val map = call.arguments as Map<*, *>
+                    timeToStart3DS(map["url"] as String)
+                } catch (e: Exception) {
+                    result.error(e.javaClass.name, e.message, e)
+                }
             }
         }
 
@@ -39,18 +47,26 @@ class YandexCheckoutFlutter {
         }
     }
 
+    fun timeToStart3DS(url:String) {
+        val intent: Intent = Checkout.create3dsIntent(
+                registrar.activity(),
+                url
+        )
+        registrar.activity().startActivityForResult(intent, 1)
+    }
+
     private fun showYandexPayment(paymentParameters: PaymentParameters) {
         val tokenizeIntent = Checkout.createTokenizeIntent(
-            registrar.activity(),
-            paymentParameters
+                registrar.activity(),
+                paymentParameters
         )
 
         registrar.activity().startActivityForResult(
-            tokenizeIntent, REQUEST_TOKENIZE_CODE)
+                tokenizeIntent, REQUEST_TOKENIZE_CODE)
     }
 
     private fun onActivityResult(
-        requestCode: Int, resultCode: Int, data: Intent?
+            requestCode: Int, resultCode: Int, data: Intent?
     ) : Boolean {
         if (requestCode != REQUEST_TOKENIZE_CODE) return false
 
@@ -59,8 +75,8 @@ class YandexCheckoutFlutter {
                 if (data != null) {
                     val tokenizationResult = Checkout.createTokenizationResult(data)
                     channelResult?.success(mapOf(
-                        "paymentMethodType" to tokenizationResult.paymentMethodType.toString(),
-                        "paymentToken" to tokenizationResult.paymentToken
+                            "paymentMethodType" to tokenizationResult.paymentMethodType.toString(),
+                            "paymentToken" to tokenizationResult.paymentToken
                     ))
                 } else {
                     channelResult?.error("RESULT_NULL", "Received tokenization result is null", null)
@@ -80,35 +96,35 @@ class YandexCheckoutFlutter {
         val map = channelArgs as Map<*, *>
 
         return PaymentParameters(
-            title = map["title"] as String,
-            subtitle = map["subtitle"] as String,
-            amount = parseAmount(map["amount"] as Map<*, *>),
-            clientApplicationKey = map["clientApplicationKey"] as String,
-            shopId = map["shopId"] as String,
-            paymentMethodTypes = (map["paymentMethodTypes"] as List<*>?)
-                ?.map { PaymentMethodType.valueOf(it as String) }
-                ?.toSet()
-                ?: PaymentMethodType.values().toSet(),
-            gatewayId = map["gatewayId"] as String?,
-            customReturnUrl = map["customReturnUrl"] as String?,
-            userPhoneNumber = map["userPhoneNumber"] as String?,
-            googlePayParameters = parseGooglePayParameters(map) ?: GooglePayParameters(),
+                title = map["title"] as String,
+                subtitle = map["subtitle"] as String,
+                amount = parseAmount(map["amount"] as Map<*, *>),
+                clientApplicationKey = map["clientApplicationKey"] as String,
+                shopId = map["shopId"] as String,
+                paymentMethodTypes = (map["paymentMethodTypes"] as List<*>?)
+                        ?.map { PaymentMethodType.valueOf(it as String) }
+                        ?.toSet()
+                        ?: PaymentMethodType.values().toSet(),
+                gatewayId = map["gatewayId"] as String?,
+                customReturnUrl = map["customReturnUrl"] as String?,
+                userPhoneNumber = map["userPhoneNumber"] as String?,
+                googlePayParameters = parseGooglePayParameters(map) ?: GooglePayParameters(),
                 savePaymentMethod = SavePaymentMethod.OFF
         )
     }
 
     private fun parseAmount(amountMap: Map<*, *>) : Amount {
         return Amount(
-            value = BigDecimal.valueOf(amountMap["value"] as Double),
-            currency = Currency.getInstance(amountMap["currency"] as String)
+                value = BigDecimal.valueOf(amountMap["value"] as Double),
+                currency = Currency.getInstance(amountMap["currency"] as String)
         )
     }
 
     private fun parseGooglePayParameters(map: Map<*, *>) : GooglePayParameters? {
         val googlePayParametersMap = (map["googlePayParameters"] as Map<*, *>?)
         val googlePayCardNetworks = (googlePayParametersMap?.get("allowedCardNetworks") as List<*>?)
-            ?.map { GooglePayCardNetwork.valueOf(it as String) }
-            ?.toSet()
+                ?.map { GooglePayCardNetwork.valueOf(it as String) }
+                ?.toSet()
 
         return if (googlePayCardNetworks != null)
             GooglePayParameters(googlePayCardNetworks)
